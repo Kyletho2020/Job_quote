ee2f967dfcf6ba21c08b07f7a28b7d61ad114023
Kyletho2020 <kyle.tho@icloud.com>
2025-07-14 14:45:15 -0700

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import OmegaMorganQuoteForm from './OmegaMorganQuoteForm';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <OmegaMorganQuoteForm />
   </React.StrictMode>
 );
