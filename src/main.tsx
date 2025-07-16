217aed876297b2aa68ae1b90396b0f0ab9b92ffa
Kyletho2020 <kyle.tho@icloud.com>
2025-07-16 08:11:17 -0700

    Create missing main.tsx entry point

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import OmegaMorganQuoteForm from './OmegaMorganQuoteForm.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <OmegaMorganQuoteForm />
+  </React.StrictMode>,
+)
